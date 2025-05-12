a1f708882cd4ca087918ee49399d9719c75b53a4
kutaiba <kalnizaemy@darsoft.net>
2025-05-12 15:56:44 +0300

    update files

--- Dockerfile
+++ Dockerfile
@@ -33,6 +33,27 @@ EXPOSE 5000
 
 CMD ["yarn", "start:dev"]
 
+# --------- Staging stage ---------
+
+FROM node:22-alpine AS staging
+
+WORKDIR /app
+
+ENV NODE_ENV=staging
+
+RUN corepack enable
+
+COPY package.json yarn.lock ./
+
+RUN yarn install 
+
+RUN yarn add -D dotenv-cli
+
+COPY --from=builder /app/dist ./dist
+
+EXPOSE 5000
+
+CMD ["yarn", "start:stag"]
 
 # --------- Production stage ---------
 FROM node:22-alpine AS production
--- src/package/index.ts
+++ src/package/index.ts
@@ -1,9 +1,7 @@
-import { RedisModule } from "@Package/cache"
 import {EnvConfigModule, WinstonLogger} from "./config"
 import { MongoConnection } from "./database"
 import { FileUploadModule, ServiceStaticModule } from "./file"
 import { EmailModule } from "./services/email/email.module"
-import {forwardRef} from "@nestjs/common";
 
 export * from "./database"
 export * from "./error"
@@ -11,6 +9,7 @@ export * from "./config"
 export * from "./auth"
 export * from "./api"
 export * from "./services"
+export * from "./utilities"
 
 export const PackageModule = [
   EnvConfigModule,
